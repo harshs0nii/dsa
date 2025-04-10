de8ead3bec619b0c41a87b177ca7d724ed58750b
harshs0nii <harshsonikcity@gmail.com>
2025-04-10 23:35:04 +0530

    gfg3

---
+++ gfg3.java
@@ -0,0 +1,15 @@
+public class gfg3 {
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+    
+            while(fast!=null && fast.next!=null){
+                fast = fast.next.next;
+                slow = slow.next;
+            }      
+            return slow;  
+        }
+    }
+    
+}
