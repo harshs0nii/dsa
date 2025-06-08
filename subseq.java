9459915383790c6161ae1719abf08c0dcb25b48a
harshs0nii <harshsonikcity@gmail.com>
2025-06-08 15:58:14 +0530

    subseq

--- subseq.java
+++ subseq.java
@@ -1,3 +1,16 @@
 public class subseq {
+    static void skip(String p , String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        skip(p, up.substring(1));
+        skip(p+ch, up.substring(1));
+            }
     
+        public static void main(String[] args) {
+        skip(" ", "harsh");
+    }
 }
+
